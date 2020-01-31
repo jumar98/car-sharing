771da4fc7fdc8dd07048653776ea4a9569612533
Julian <jumar98@hotmail.com>
2020-01-30 22:47:01 -0500

    [feat] Validate members limit and add the user that create the circle as admin from this.

--- cride/circles/serializers.py
+++ cride/circles/serializers.py
@@ -3,11 +3,30 @@ from cride.circles.models import Circle
 
 class CircleModelSerializer(serializers.ModelSerializer):
 
+    members_limit = serializers.IntegerField(
+        required=False,
+        min_value=10,
+        max_value=32000
+    )
+    is_limited = serializers.BooleanField(default=False)
     class Meta:
 
         model = Circle
         fields = (
             'id', 'name', 'slug_name', 'about',
             'picture', 'rides_offered', 'rides_taken',
-            'is_verified', 'is_public', 'members_limit'
+            'is_verified', 'is_public', 'members_limit', 'is_limited'
         )
+        read_only_fields = (
+            'is_public',
+            'verified',
+            'rides_offered',
+            'rides_taken'
+        )
+
+    def validate(self, data):
+        members_limit = data.get('members_limit', None)
+        is_limited = data.get('is_limited', False)
+        if is_limited ^ bool(members_limit):
+            raise serializers.ValidationError('If circle is limited, a member limit must be provided')
+        return data
--- cride/circles/views/circles.py
+++ cride/circles/views/circles.py
@@ -1,5 +1,5 @@
 from rest_framework import viewsets
-from cride.circles.models import Circle
+from cride.circles.models import Circle, Membership
 from cride.circles.serializers import CircleModelSerializer
 from rest_framework.permissions import IsAuthenticated
 
@@ -15,3 +15,16 @@ class CircleViewSet(viewsets.ModelViewSet):
         if self.action == 'list':
             return queryset.filter(is_public=True)
         return queryset
+
+    def perform_create(self, serializer):
+
+        circle = serializer.save()
+        user = self.request.user
+        profile = user.profile
+        Membership.objects.create(
+            user=user,
+            profile=profile,
+            circle=circle,
+            is_admin=True,
+            remaining_invitation=10
+        )
