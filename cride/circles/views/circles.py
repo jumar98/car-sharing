81ed7f90b21b1b7671f2716413098dcbe0577943
Julian <jumar98@hotmail.com>
2020-01-31 22:35:17 -0500

    [feat] Create a custom permission to avoid update, to users that aren't admin 	of a circle

--- cride/circles/views/circles.py
+++ cride/circles/views/circles.py
@@ -1,23 +1,29 @@
-from rest_framework import viewsets
+from rest_framework import mixins, viewsets
 from cride.circles.models import Circle, Membership
 from cride.circles.serializers import CircleModelSerializer
 from rest_framework.permissions import IsAuthenticated
-
-class CircleViewSet(viewsets.ModelViewSet):
+from cride.circles.permissions.circles import IsCircleAdmin
+class CircleViewSet(mixins.CreateModelMixin,
+                    mixins.RetrieveModelMixin,
+                    mixins.UpdateModelMixin,
+                    mixins.ListModelMixin,
+                    viewsets.GenericViewSet):
 
     queryset = Circle.objects.all()
     serializer_class = CircleModelSerializer
-    permission_classes = (IsAuthenticated,)
 
     def get_queryset(self):
-
         queryset = Circle.objects.all()
         if self.action == 'list':
             return queryset.filter(is_public=True)
         return queryset
 
+    def get_permissions(self):
+        permissions = [IsAuthenticated]
+        if self.action in ['update', 'partial_update']:
+            permissions.append(IsCircleAdmin)
+        return [permission() for permission in permissions]
     def perform_create(self, serializer):
-
         circle = serializer.save()
         user = self.request.user
         profile = user.profile
